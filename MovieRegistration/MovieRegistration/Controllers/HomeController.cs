1cc1ac4c02088c50fa7364659b21a0e41ef9622c
Maronda <marondaanderson@gmail.com>
2021-10-28 12:52:57 -0400

    intial commit

---
+++ MovieRegistration/MovieRegistration/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieRegistration.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRegistration.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Registration()
+        {
+            return View(new Movie());
+        }
+        public IActionResult MovieDisplay(Movie film)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(film);
+            }
+            else
+            {
+                return View("Registration", film);
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
